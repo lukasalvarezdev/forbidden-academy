a9ebcb7baf2d584e6b6807f21a9fbee933f202a2
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-06-24 19:47:26 -0500

    Created useLessons hook

---
+++ src/entities/shared-lesson/hooks/useLesson.ts
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import { Lesson } from '../services'
+
+export function useLesson() {
+  const [sections, setSections] = React.useState([
+    { name: 'First section', id: '2jdsa', number_of_lessons: 29 },
+    { name: 'First section', id: '2jdsas', number_of_lessons: 29 },
+  ])
+
+  const [lessons, setLessons] = React.useState<Lesson[]>([
+    {
+      name: 'First',
+      description: 'Hello world',
+      id: 'sdads',
+      announcements: [
+        {
+          title: 'pepe',
+          body: 'sd',
+          posted_time: 'sd',
+        },
+      ],
+      last_modified_time: 'sd',
+      posted_time: 'sda',
+      questions: [],
+      section: '2jdsa',
+    },
+  ])
+
+  function createSection(name: string) {
+    setSections(sections => [...sections, { name, id: 'das', number_of_lessons: 22 }])
+  }
+
+  function updateSection(name: string, index: number) {
+    const sectionsCopy = [...sections]
+    sectionsCopy[index] = { ...sectionsCopy[index], name }
+    setSections(sectionsCopy)
+  }
+
+  function createLesson(lesson: Lesson) {
+    setLessons(lessons => [...lessons, lesson])
+  }
+
+  function updateLesson(rewrites: Partial<Lesson>, index: number) {
+    const lessonsCopy = [...lessons]
+    lessonsCopy[index] = { ...lessonsCopy[index], ...rewrites }
+    setLessons(lessonsCopy)
+  }
+
+  return {
+    sections,
+    setSections,
+    lessons,
+    setLessons,
+    createSection,
+    updateSection,
+    createLesson,
+    updateLesson,
+  }
+}
