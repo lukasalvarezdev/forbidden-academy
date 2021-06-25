21c48367e8f429167ea4926e95a104b96cf8f095
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-06-24 20:37:18 -0500

    Moved sections and lessons into its respective files

--- src/entities/course/layouts/CourseBody.tsx
+++ src/entities/course/layouts/CourseBody.tsx
@@ -1,10 +1,9 @@
-import { useLesson } from 'src/entities/shared-lesson/services'
-import WhatYouWillLearn from '../components/WhatYouWillLearn'
 import { useEditableInput } from '../services'
+import WhatYouWillLearn from '../components/WhatYouWillLearn'
+import Sections from './Sections'
 
 const CourseBody = () => {
   const handleChange = useEditableInput()
-  const { sections, lessons, createSection, createLesson } = useLesson('pepe')
 
   return (
     <>
@@ -16,45 +15,7 @@ const CourseBody = () => {
 
         <WhatYouWillLearn />
       </form>
-      <div>
-        {sections.map((section, index) => (
-          <div key={index}>
-            <input type="text" defaultValue={section.name} />
-            <p>{section.number_of_lessons} Lessons</p>
-
-            {lessons.map((lesson, index) =>
-              lesson.section === section.id ? (
-                <div key={index}>
-                  <input type="text" defaultValue={lesson.name} />
-                  <input type="text" defaultValue={lesson.description} />
-                  <p>{lesson.posted_time}</p>
-                </div>
-              ) : null,
-            )}
-          </div>
-        ))}
-
-        <button
-          onClick={e => {
-            e.preventDefault()
-            createSection('El pepe 99')
-          }}
-        >
-          Create section
-        </button>
-        <button
-          onClick={e => {
-            e.preventDefault()
-            createLesson({
-              name: 'Class 1',
-              description: 's',
-              section: '2jdsa',
-            })
-          }}
-        >
-          Create lesson
-        </button>
-      </div>
+      <Sections />
     </>
   )
 }
