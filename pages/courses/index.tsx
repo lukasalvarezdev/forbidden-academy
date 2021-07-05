4c4f59c99e49c4496a25393960a4baef07a6a260
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-07-05 10:45:28 -0500

    Styled lesson page

---
+++ pages/courses/index.tsx
@@ -0,0 +1,11 @@
+import CoursesProvider from 'src/entities/course/store/context'
+import CoursePage from 'src/entities/course/views/CoursePage'
+import Header from 'src/layouts/Header'
+
+export default function CreateCoursePage() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
