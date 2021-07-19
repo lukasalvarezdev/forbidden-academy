837f3c0904d49b23ef21243776810011927706e7
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-07-19 18:43:37 -0500

    Adding styles to selects

--- frontend/src/entities/course/layouts/ManageCourseForm.tsx
+++ frontend/src/entities/course/layouts/ManageCourseForm.tsx
@@ -33,11 +33,24 @@ const MobileCourseLayout = () => {
         </div>
         <div className="field mb-20">
           <label htmlFor="pepe">Skill level</label>
-          <input type="text" name="skill_level" defaultValue={course.skill_level} />
+          <select
+            name="skill_level"
+            defaultValue={course.skill_level}
+            className="select-primary"
+          >
+            <option value="">Select an option</option>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="advanced">Advanced</option>
+          </select>
         </div>
         <div className="field mb-20">
           <label htmlFor="pepe">Language</label>
-          <input type="text" name="language" defaultValue={course.language} />
+          <select name="language" defaultValue={course.language} className="select-primary">
+            <option value="">Select a language</option>
+            <option value="spanish">Spanish</option>
+            <option value="english">English</option>
+          </select>
         </div>
         <div className="field mb-20">
           <label htmlFor="pepe">Price</label>
@@ -59,10 +72,16 @@ export const CourseContainer = styled.div`
   width: 95%;
   margin: 3rem auto;
 
-  .field textarea {
-    min-height: 15rem;
-    line-height: 1.7;
-    padding-top: 10px;
-    resize: none;
+  .field {
+    textarea {
+      min-height: 15rem;
+      line-height: 1.7;
+      padding-top: 10px;
+      resize: none;
+    }
+
+    .select-primary {
+      padding-right: 15px;
+    }
   }
 `
