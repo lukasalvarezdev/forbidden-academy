2e48400876308d6000731669137febe4ec08ca58
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-07-19 18:28:19 -0500

    Adding styles to create manage course

--- frontend/src/entities/course/layouts/ManageCourseForm.tsx
+++ frontend/src/entities/course/layouts/ManageCourseForm.tsx
@@ -10,28 +10,37 @@ const MobileCourseLayout = () => {
   const { updateCourse, course, handleSubmit } = useCourses()
 
   return (
-    <CourseContainer>
+    <CourseContainer className="p-20 border-radius-primary normal-shadow">
       <form
         onChange={(e: React.ChangeEvent<HTMLFormElement>) =>
           updateCourse({ [e.target.name]: e.target.value })
         }
       >
-        <div className="mb-20">
+        <h3 className="mb-20">
+          {id ? 'Edit the information of your course' : 'Fill course info'}
+        </h3>
+        <div className="field mb-20">
+          <label htmlFor="pepe">Title</label>
           <input type="text" name="title" defaultValue={course.title} />
         </div>
-        <div className="mb-20">
-          <input type="text" name="description" defaultValue={course.description} />
+        <div className="field mb-20">
+          <label htmlFor="pepe">Description</label>
+          <textarea name="description" defaultValue={course.description}></textarea>
         </div>
-        <div className="mb-20">
+        <div className="field mb-20">
+          <label htmlFor="pepe">Short description</label>
           <input type="text" name="short_description" defaultValue={course.short_description} />
         </div>
-        <div className="mb-20">
+        <div className="field mb-20">
+          <label htmlFor="pepe">Skill level</label>
           <input type="text" name="skill_level" defaultValue={course.skill_level} />
         </div>
-        <div className="mb-20">
+        <div className="field mb-20">
+          <label htmlFor="pepe">Language</label>
           <input type="text" name="language" defaultValue={course.language} />
         </div>
-        <div className="mb-20">
+        <div className="field mb-20">
+          <label htmlFor="pepe">Price</label>
           <input type="text" name="price" defaultValue={course.price} />
         </div>
 
@@ -45,4 +54,15 @@ const MobileCourseLayout = () => {
 
 export default MobileCourseLayout
 
-export const CourseContainer = styled.div``
+export const CourseContainer = styled.div`
+  background-color: #fff;
+  width: 95%;
+  margin: 3rem auto;
+
+  .field textarea {
+    min-height: 15rem;
+    line-height: 1.7;
+    padding-top: 10px;
+    resize: none;
+  }
+`
