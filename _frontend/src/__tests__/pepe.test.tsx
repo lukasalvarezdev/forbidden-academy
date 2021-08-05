08eadca555ce853fd5cd02082b306930791add45
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-08-05 08:23:30 -0500

    remove: mock tests

--- _frontend/src/__tests__/pepe.test.tsx
+++ _frontend/src/__tests__/pepe.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { render } from 'src/testUtils'
 import Pepe from '@/components/Pepe'
 
@@ -8,3 +7,18 @@ describe('Home page', () => {
     expect((await findByText('El pepe')).innerHTML).toBe('El pepe')
   })
 })
+
+// describe('Home page', () => {
+//   it('matches snapshot', async () => {
+//     const { findByText } = render(<Home />, {})
+//     // expect(asFragment()).toMatchSnapshot()
+//     expect((await findByText('El pepe')).innerHTML).toBe('El pepe')
+//   })
+
+//   // it('clicking button triggers alert', () => {
+//   //   const { getByText } = render(<Home />, {})
+//   //   window.alert = jest.fn()
+//   //   fireEvent.click(getByText('Test Button'))
+//   //   expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+//   // })
+// })
