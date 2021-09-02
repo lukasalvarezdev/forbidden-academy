fec8383faf0361fe8878db16089420a2acacf638
Lukas Alvarez <53347228+lukasalvarezdev@users.noreply.github.com>
2021-09-02 08:07:48 -0500

    build(frontend): update to next 11.1.2
    Update due to a bug importing global css in _app.tsx see: https://github.com/vercel/next.js/issues/28579

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import { AppProps } from 'next/app'
-import '../styles/variables.css'
 import '../styles/globals.css'
-import '../styles/resets.css'
-import '../styles/lib.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
